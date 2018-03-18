be7f4d793a909ff3f5511de71d2cb7510119512a
Marco Lopez <marcolopezpe@outlook.com>
2018-03-18 11:55:26 -0500

    primero envio de ejercicios

---
+++ fundamentos-java/leccion03/EjemploIf/src/sentenciascontrol/EjemploIf.java
@@ -0,0 +1,14 @@
+package sentenciascontrol;
+
+public class EjemploIf {
+
+    public static void main(String[] args) {
+        //Ejemplo If
+        int x = 10;
+        
+        if (x < 20) {
+            System.out.println("X menor que 20\n");
+        }
+    }
+
+}
---
+++ fundamentos-java/leccion03/ManejoCadenasBasico/src/manejocadenasbasico/ManejoCadenasBasico.java
@@ -0,0 +1,18 @@
+package manejocadenasbasico;
+
+public class ManejoCadenasBasico {
+
+    public static void main(String[] args) {
+        String nombre = "Karla";
+        String apellido = "Esparza";
+        
+        System.out.println("Concatenacion: " + nombre + apellido);
+        System.out.println("Nueva Linea: \n" + nombre + " " + apellido);
+        System.out.println("Tabulador: \t" + nombre + " " + apellido);
+        System.out.println("Retroceso: \b" + nombre + " " + apellido);
+        System.out.println("Retorno de carro: \r" + nombre + " " + apellido);
+        System.out.println("Comilla simple: \'" + nombre + " " + apellido + "\'");
+        System.out.println("Comilla doble: \"" + nombre + " " + apellido + "\"");
+    }
+    
+}
